6d9aa63512b85b58339b3dd768b5026772a268b8
Daniel Frimu <frimudaniel17@gmail.com>
2022-01-30 16:51:38 +0100

    407

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Todos from "./components/Todos";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 };
 
 export default App;
