27740bf53381347c09c4f92dbabf3af0ac204e92
Daniel Frimu <frimudaniel17@gmail.com>
2022-01-30 21:21:34 +0100

    414

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 const App = () => {
-  const todos = [new Todo("Learn React"), new Todo("Leart TypeScript")];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const addTodoHandler = (todoText: string) => {};
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
