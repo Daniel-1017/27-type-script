9b0cbf567885576875916ba55adaf615d1a3c465
Daniel Frimu <frimudaniel17@gmail.com>
2022-01-30 20:19:09 +0100

    412

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,24 @@
 import React, { useRef } from "react";
 
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+    // ! means that you are sure that you will get the value
+    // ? means that if you don't get the value you'll get null
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Todo Form</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
